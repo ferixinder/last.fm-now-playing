d186b1791e58f4075c1b1ec7298b798e45874c5e
ferixinder <50833760+ferixinder@users.noreply.github.com>
2021-05-05 15:16:19 +0300

    Create song.js

---
+++ song.js
@@ -0,0 +1,44 @@
+getSong();
+
+setInterval(function() {
+    getSong()
+}, 2000);
+
+function getSong() {
+    jQuery.ajax({
+        url: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ferixinder&api_key=ccdf053dcd5b4979d4f7d45fcb1af136&format=json",
+        type: "GET",
+        dataType: "json",
+        async: false,
+        success: function(data) {
+            showSpotifyWidget(data.recenttracks.track[0]);
+        }
+    });
+}
+
+function showSpotifyWidget(lastSong) {
+    var lastSongName = lastSong.artist["#text"] + " | " + lastSong.name;
+    var lastSongAlbum = "> " + lastSong.album["#text"];
+
+    if (lastSongName.length > 29) {
+        lastSongName = lastSongName.substring(0, 29) + "...";
+    }
+
+    if (lastSongAlbum.length > 25) {
+        lastSongAlbum = lastSongAlbum.substring(0, 19) + "...";
+    }
+
+
+
+    if (lastSong.hasOwnProperty('@attr')) {
+        $("#nowLink").attr("href", lastSong.url);
+        $("#nowWidgetIMG").attr("src", lastSong.image[3]["#text"]);
+        $('#nowSong').text(lastSongName);
+        $('#nowAlbum').text(lastSongAlbum);
+    } else {
+        $("#nowLink").attr("href", lastSong.url);
+        $("#nowWidgetIMG").attr("src", lastSong.image[3]["#text"]);
+        $('#nowSong').text(lastSongName)
+        $('#nowAlbum').text(lastSongAlbum + "(Şu anda dinlemiyor)");
+    }
+}
