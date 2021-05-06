728d4a6af4477ceeeb7ce9edff96756e1b3da24a
ferixinder <50833760+ferixinder@users.noreply.github.com>
2021-05-06 16:27:50 +0300

    Update song.js
    image added to music without image

--- song.js
+++ song.js
@@ -19,6 +19,7 @@ function getSong() {
 function showSpotifyWidget(lastSong) {
     var lastSongName = lastSong.artist["#text"] + " | " + lastSong.name;
     var lastSongAlbum = "> " + lastSong.album["#text"];
+    var lastSongUrl = lastSong.image[3]["#text"];
 
     if (lastSongName.length > 29) {
         lastSongName = lastSongName.substring(0, 29) + "...";
@@ -27,17 +28,21 @@ function showSpotifyWidget(lastSong) {
     if (lastSongAlbum.length > 25) {
         lastSongAlbum = lastSongAlbum.substring(0, 19) + "...";
     }
+    
+    if (lastSongUrl == "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png") {
+        lastSongUrl = "https://sc04.alicdn.com/kf/U866b4bda76d249f8920c19093500a98fb.jpg"
+    }
 
 
 
     if (lastSong.hasOwnProperty('@attr')) {
         $("#nowLink").attr("href", lastSong.url);
-        $("#nowWidgetIMG").attr("src", lastSong.image[3]["#text"]);
+        $("#nowWidgetIMG").attr("src", lastSongUrl);
         $('#nowSong').text(lastSongName);
         $('#nowAlbum').text(lastSongAlbum);
     } else {
         $("#nowLink").attr("href", lastSong.url);
-        $("#nowWidgetIMG").attr("src", lastSong.image[3]["#text"]);
+        $("#nowWidgetIMG").attr("src", lastSongUrl);
         $('#nowSong').text(lastSongName)
         $('#nowAlbum').text(lastSongAlbum + "(Not listening now)");
     }
